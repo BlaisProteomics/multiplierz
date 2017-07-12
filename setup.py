a6012abeccd59775666104f6d179f9d98b569eb1
MaxAlex <w.max.alexander@gmail.com>
2017-07-12 18:45:22 -0400

    Added MSFileReader installer to package data.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 
 setup(name = 'multiplierz',
-      version = '2.0',
+      version = '2.0.1',
       description = 'The multiplierz proteomics package',
       author = 'William Max Alexander (et al.)',
       author_email = 'williamM_alexander@dfci.harvard.edu',
@@ -20,7 +20,7 @@ setup(name = 'multiplierz',
       package_data = {'multiplierz.mzAPI':['mzAPI/agilentdlls/*',
                                            'mzAPI/t2ddlls/*',
                                            'mzAPI/wiffdlls/*'],
-                      'multiplierz':['unimod.sqlite', '_msparser.pyd']},
+                      'multiplierz':['unimod.sqlite', '_msparser.pyd', 'mzAPI/MSFileReader_x86_x64_v3.0SP3.exe']},
       include_package_data=True,
       install_requires = ['numpy', 'comtypes', 'matplotlib', 'pypiwin32',
                           'openpyxl', 'xlrd', 'xlwt', 'requests'], # Removed 'lxml'.
